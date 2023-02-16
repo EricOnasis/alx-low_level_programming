a340f1bf605c9946326e4646829fd5e304f1599f
Ericonasis <ericonasis@gmail.com>
2023-02-16 15:59:33 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * Main: Prints the sentence
+ * Returns 0 if the program exited properly
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
