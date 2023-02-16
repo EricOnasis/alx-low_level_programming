c88cd7dc8a96aeb31d563390c441a86b938ffba9
Ericonasis <ericonasis@gmail.com>
2023-02-16 15:54:38 -0800

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints  "Programming is like building a multilingual puzzle
- * 
+ * main - prints "Programming is like building a multilingual puzzle
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
