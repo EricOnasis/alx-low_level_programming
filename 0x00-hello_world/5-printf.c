842528bdf7abb9c13fe146712382a7ae9a2ee6b8
Ericonasis <ericonasis@gmail.com>
2023-02-16 16:04:06 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main: Prints with proper grammar, but the outcome is a piece of art
- * returns: 0 if the program exited properly
+ * main - prints with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly
  */
 int main(void)
 {
