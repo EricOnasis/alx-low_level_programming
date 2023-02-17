b94cd53e9180f160716595009cd18e041ed0a7b1
Ericonasis <ericonasis@gmail.com>
2023-02-17 11:48:17 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * skips letter q and e
+ * then a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char test = 'a';
+
+	while (test <= 'z')
+	{
+		if (test != 'e' && test != 'q')
+		{
+			putchar(test);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
