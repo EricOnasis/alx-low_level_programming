2baf176a1a09f98ad926e3026023bdc94706897b
Ericonasis <ericonasis@gmail.com>
2023-02-21 07:32:28 -0800

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks for lowecase
+ * @c: the character
+ * Return: 1 if c is lowercase
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	return (1);
+	else
+	return (0);
+}
