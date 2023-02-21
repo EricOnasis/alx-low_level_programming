0b31d18dbb867179b4d3b3ad85f615fecfa480a3
Ericonasis <ericonasis@gmail.com>
2023-02-21 06:27:55 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowecase
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a >= 'a'; a <= 'z'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+
+}
