57910cc418f4661f73f06af28799e252019e1173
Ericonasis <ericonasis@gmail.com>
2023-02-23 10:34:36 -0800

    isUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: is the input
+ * Returns: 1 if its uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
