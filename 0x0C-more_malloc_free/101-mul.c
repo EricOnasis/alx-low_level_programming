9773fa69c6d5cea43a40e4875dc732bc71498247
Onasis <ericonasis@gmail.com>
2023-03-17 01:25:17 +0300

    5-Correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,25 +6,42 @@
  * @argv: args
  * Return: int
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
-
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
+		{
+		printf("Error\n");
+		exit(98);
+		}
+
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+
+	for (int i = 0; num1[i] != '\0'; i++)
 	{
-	for (j = 0; argv[i][j] != '\0'; j++)
-	{
-		if (argv[i][j] > 57 || argv[i][j] < 48)
-		{  printf("Error\n");
-		exit(98); }
+		if (!isdigit(num1[i]))
+		{
+		printf("Error\n");
+		exit(98);
+		}
 	}
 
+	for (int i = 0; num2[i] != '\0'; i++)
+	{
+		if (!isdigit(num2[i]))
+		{
+		printf("Error\n");
+		exit(98);
+		}
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+
+	int result = atoi(num1) * atoi(num2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
