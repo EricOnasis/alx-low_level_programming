314df5116bb80161d0a2025b663eb2d1c5797408
Eric Onasis <ericonasis@gmail.com>
2023-04-06 20:11:04 +0300

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,8 @@
  * @b: The string with binary number
  * Return: Returns the converted number
  */
-unsigned int binary_to_uint(const char *b){
+unsigned int binary_to_uint(const char *b)
+{
 	int conv;
 	unsigned int deci = 0;
 
@@ -13,10 +14,10 @@ unsigned int binary_to_uint(const char *b){
 		return (0);
 
 	for (conv = 0; b[conv]; conv++)
-		{	
-			if (b[conv] < '0' || b[conv] > '1')
-				return (0);
-			deci = 2 * deci + (b[conv] - '0');
+		{
+		if (b[conv] < '0' || b[conv] > '1')
+			return (0);
+		deci = 2 * deci + (b[conv] - '0');
 		}
 	return (deci);
 }
