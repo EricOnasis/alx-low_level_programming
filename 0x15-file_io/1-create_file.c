0b1ad99ba1c796e5738a12cab7f984cd8f1d4e42
Eric Onasis <ericonasis@gmail.com>
2023-04-10 23:57:04 +0300

    A function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int len = 0;
-	int opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	int opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	int wr = write(opn, text_content, len);
 
 
