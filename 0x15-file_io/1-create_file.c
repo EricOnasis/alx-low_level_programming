9c88a3ef1b24b6bc88efc5fa1f3ea04de462d164
Eric Onasis <ericonasis@gmail.com>
2023-04-11 00:05:59 +0300

    Task 1 Correction2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR|S_IWUSR);
-        wr = write(opn, text_content, len);
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	wr = write(opn, text_content, len);
 
 
 	if (opn == -1 || wr == -1)
