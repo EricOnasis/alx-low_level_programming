3ed6950a41f490b9d0afb9461ec28bc6941945a4
Eric Onasis <ericonasis@gmail.com>
2023-04-10 09:32:12 +0300

    Flip Bits correction

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		if ((m & 1) != (n & 1))
 			flip += 1;
 		n = n >> 1;
-        m = m >> 1;
+		m = m >> 1;
 	}
 	return (flip);
 }
