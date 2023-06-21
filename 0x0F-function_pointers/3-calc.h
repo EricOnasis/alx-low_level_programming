d7e6d7de9ae630c6cc78e7f9ae1607d1362e3881
Eric Onasis <ericonasis@gmail.com>
2023-06-21 20:09:53 +0300

    Write a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,12 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct op - The function
+ * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
@@ -19,4 +23,6 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
+
